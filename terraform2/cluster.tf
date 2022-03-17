2ca97f94f0b6262734a3fa92b1de9ce24897c40c
bhilleri <benjaminh@live.fr>
2022-03-17 20:11:26 +0100

    kubernetes

---
+++ terraform2/cluster.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
+    }
+  }
+}
+
+resource "virtualbox_vm" "node" {
+  count     = 1
+  name      = "minikube"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20220310.0.0/providers/virtualbox.box"
+  cpus      = 2
+  memory    = "512 mib"
+
+  network_adapter {
+    type           = "nat"
+  }
+}
+
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
